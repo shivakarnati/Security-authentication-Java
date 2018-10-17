9d6ebe4e884eea2c612492d054944cec9832da20
shivakarnati <38187230+shivakarnati@users.noreply.github.com>
2018-10-17 13:23:07 +0530

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,20 @@
+package com.codefelix2.springbootquickstart.courseapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+     @RequestMapping(value = "")
+    @ResponseBody
+      public String index(){
+         return "hello world";
+     }
+
+     @RequestMapping(value = "goodbye")
+    @ResponseBody
+    public String goodbye(){
+         return "Goodbye";
+     }
+}
